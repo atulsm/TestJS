b1a1d5e369430ed690505d77e48e1e4333f860b4
Atul Soman <atul.soman@microfocus.com>
2019-04-08 09:31:11 +0530

    Testing var declaration

---
+++ test_js/TestNoVarDeclaration.js
@@ -0,0 +1,21 @@
+//testing to see what happens if you dont declate a variable and use it
+
+(()=>{
+    a=2; //no var, let, const
+})();
+
+//after function invocation it has global scope
+console.log(a);
+
+var test = function() {
+    let b = 2;
+
+    if(true){
+       // let b = 3;
+        console.log(b);
+    }
+    console.log(b);
+
+}
+
+test();
